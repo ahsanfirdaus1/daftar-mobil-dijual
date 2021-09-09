65a6b580f9fac8c72f7bfd5ca2e646a3d650bff4
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-09 21:20:02 +0700

    Menghapus parameter jenis mobil

--- carList.py
+++ carList.py
@@ -1,17 +1,13 @@
 class car:
     
-    def __init__(self, namaMobil , jenisMobil , merekMobil , tahunMobil , hargaMobil):
+    def __init__(self, namaMobil  , merekMobil , tahunMobil , hargaMobil):
         self.__namaMobil = namaMobil
-        self.__jenisMobil = jenisMobil
         self.__merekMobil = merekMobil
         self.__tahunMobil = tahunMobil
         self.__hargaMobil = hargaMobil 
     
     def set_namaMobil (self, namaMobil):
         self.__namaMobil = namaMobil
-    
-    def set_jenisMobil (self, jenisMobil):
-        self.__jenisMobil = jenisMobil
     
     def set_merekMobil (self, merekMobil):
         self.__merekMobil = merekMobil
@@ -24,9 +20,6 @@ class car:
     
     def get_namaMobil (self):
         return self.__namaMobil
-
-    def get_jenisMobil (self):
-        return self.__jenisMobil
 
     def get_merekMobil (self):
         return self.__merekMobil
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import carList
 
-print("Hello Bambang")
+def main():
+    
+    print("Program Daftar Mobil")
+    print("xxxxxxxxxxxxxxxxxxxx")
+    print()
+    userName = input("Nama Anda: ")
+    print()
+    print()
+
+    namaMobil = input("Nama Mobil: ")
+    print()
+    jenisMobil = input("Jenis Mobil: ")
+    print()
+    merekMobil = input("Merek Mobil: ")
+    print()
+    tahunMobil = input("Tahun: ")
+    print()
+    hargaMobil = input("Harga: ")
+    print()
+
+    finalMobil = carList.car(namaMobil, jenisMobil, merekMobil, tahunMobil, hargaMobil )
+    print()
+
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print(f"Output Yang diinput oleh: \n{userName}\n")
+    print(f"{namaMobil} ")
+
+main()
+
