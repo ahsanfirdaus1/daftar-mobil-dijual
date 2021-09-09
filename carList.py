f5a0fac5d3bbc1f334e200a91de0401357e19a2c
ahsanfirdaus1 <ahsanfirdaus1@gmail.com>
2021-09-09 17:41:13 +0700

    Menambahkan function __init__ dengan parameter nya

--- carList.py
+++ carList.py
@@ -1,1 +1,11 @@
-class car: 
+class car:
+    
+    def __init__(self, namaMobil , jenisMobil , merekMobil , tahunMobil , hargaMobil):
+        self.__namaMobil = namaMobil
+        self.__jenisMobil = jenisMobil
+        self.__merekMobil = merekMobil
+        self.__tahunMobil = tahunMobil
+        self.__hargaMobil = hargaMobil 
+        
+
+
